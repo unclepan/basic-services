822ad11e2b3b34dc682d6799f8825a41f43c8cf5
yangpan12 <738912168@qq.com>
2020-04-28 11:30:10 +0800

    fix:数据库调整

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	secret: 'antcp-jwt-secret',
-	connectionStr: 'mongodb://pan:Yp654321@localhost:27017/antcp?authSource=antcp',
+	connectionStr: 'mongodb://uncle:Yp123456@localhost:27017/antcp?authSource=admin',
 	connectionCrawlerStr: 'mongodb://localhost:27017/crawler',// 爬虫数据库
 	redis:{
 		get host(){
