8367e5c630b79bcbaed2dec0123c7047152dd948
yangpan12 <738912168@qq.com>
2020-06-06 22:29:29 +0800

    fix:调整服务

--- admin-template/static/js/chunk-2d0ba0e9.badf5367.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ba0e9"],{"361a":function(n,e,t){"use strict";t.r(e);var u=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[t("router-view")],1)},o=[],c={data:function(){return{}},components:{},mounted:function(){},methods:{}},r=c,a=t("2877"),i=Object(a["a"])(r,u,o,!1,null,null,null);e["default"]=i.exports}}]);
-//# sourceMappingURL=chunk-2d0ba0e9.badf5367.js.map
